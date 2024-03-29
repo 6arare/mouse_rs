2d20d8cd017a52b324374fab4d91dcd15a7ae390
barare <blue90915@mail.ir>
2024-03-30 01:31:33 +0330

    working

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,52 @@ use hookmap::device::{self, mouse};
 // use inputbot::{MouseCursor, KeybdKey::*};
 
 fn main() {
-	loop{
-		// QKey.bind(||{std::process::exit(0)});
+	// QKey.bind(||{std::process::exit(0)});
+	// inputbot::handle_input_events();
 		// LControlKey.bind(||{
 		// // let mouse_position=inputbot::MouseCursor::pos();
-		// MouseCursor::move_rel(384, 0);
-		// sleep(Duration::from_millis(10));
-		// MouseCursor::move_rel(-768, 0);
-		// sleep(Duration::from_millis(10));
-		// MouseCursor::move_rel(384, 0);	
-		// sleep(Duration::from_millis(10));
-		// // MouseCursor::move_rel(mouse_position.0, mouse_position.1);
-		// sleep(Duration::from_millis(10));
-		// println!("x :{:?} y : {:?}", mouse_position.0,mouse_position.0+384);
-	// });
-		
-// inputbot::handle_input_events();}	
-		// let  pos:(i32,i32) = mouse::get_position();
+	// loop{
+	// 	MouseCursor::move_rel(384, 0);
+	// 	sleep(Duration::from_millis(20));
+	// 	MouseCursor::move_rel(-768, 0);
+	// 	sleep(Duration::from_millis(20));
+	// 	MouseCursor::move_rel(384, 0);	
+	// 	sleep(Duration::from_millis(20));
+	// 	}}	
+	
+
 	// 	if pos.0<384{
-	// 	let  left_mouse_x:i32=0 ;
-	// 	let  right_mouse_x:i32=pos.0+384;
-			mouse::move_relative(384,0);
-				sleep(Duration::from_millis(10));
-			mouse::move_relative(-768,0);
-				sleep(Duration::from_millis(10));	
-			mouse::move_relative(384,0);
-				sleep(Duration::from_millis(10));	
-	// 		mouse::move_relative(pos.0,pos.1);
+		// 	let  left_mouse_x:i32=0 ;
+		// 	let  right_mouse_x:i32=pos.0+384;
+		loop{
+			let  pos:(i32,i32) = mouse::get_position();
+			if pos.0 <384 {
+				mouse::move_relative(384,0);
+				sleep(Duration::from_millis(20));
+				mouse::move_relative(-384,0);
+				sleep(Duration::from_millis(20));	
+			}
+			else if pos.0 > 1151 {
+				mouse::move_relative(-384,0);
+				sleep(Duration::from_millis(20));
+				mouse::move_relative(384,0);
+				sleep(Duration::from_millis(20));	
+			}
 			
+			else{
+				mouse::move_relative(384,0);
+					sleep(Duration::from_millis(20));
+				mouse::move_relative(-768,0);
+					sleep(Duration::from_millis(20));	
+				mouse::move_relative(384,0);
+					sleep(Duration::from_millis(20));
+				}
+			}
+			}
 	// 		continue;
-		}
 		
-	// 	if pos.0>1151{
+		
+	// 	if pos.0>1151{		
 	// 		let  left_mouse_x:i32=pos.0-384 ;
 	// 		let  right_mouse_x:i32=1535;
 	// 		mouse::move_relative(right_mouse_x,pos.1);
@@ -52,10 +66,3 @@ fn main() {
 	// 		mouse::move_relative(left_mouse_x,pos.1);
 	// 		mouse::move_relative(pos.0,pos.1);
 	// 		continue;
-			
-
-	}
-
-
-
-	
